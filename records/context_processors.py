623f85bb3b47f5baf39eba60e346a8bd59cb497f
EarlClaro <earlclaro@gmail.com>
2024-10-12 21:48:39 +0800

    update

--- records/context_processors.py
+++ records/context_processors.py
@@ -10,7 +10,7 @@ def subscription_status(request):
             # Use locked_plan_name to show the subscribed plan, not the updated one
             plan_name = subscription.locked_plan_name if subscription.locked_plan_name else 'Free'
 
-            if subscription.is_near_end() and subscription.plan_id_id in [2, 3]:
+            if subscription.is_near_end() and subscription.plan_id_id in [1, 2, 3]:
                if not request.session.get('modal_shown', False):
                    show_modal = True
                    request.session['modal_shown'] = True
